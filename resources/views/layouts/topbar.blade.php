c4845889f070f13638ab228ad9b9c9dc99ce67c4
Mariano <marianosciacco@gmail.com>
2020-03-21 19:10:39 +0100

    Update topbar.blade.php

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -45,8 +45,9 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600">{{Auth::user()->name}} {{Auth::user()->surname}}</span>
-                <img class="img-profile rounded-circle" src="https://i.postimg.cc/L5QPSt76/avatar.jpg">
+                <i class="far fa-user text-gray-600"></i>
+                <span class="mx-2 d-none d-lg-inline text-gray-600">{{Auth::user()->name}} {{Auth::user()->surname}}</span>
+                <i class="fas fa-caret-down text-gray-600"></i>
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
