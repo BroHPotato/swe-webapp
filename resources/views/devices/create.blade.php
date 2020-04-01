9f76631d161e69785942f966f84fbb61ecbda61b
Alessandro <tommasinalessandro@gmail.com>
2020-04-01 18:01:15 +0200

    Update struttura pagina di creazione

--- resources/views/devices/create.blade.php
+++ resources/views/devices/create.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 @section('breadcrumbs', Breadcrumbs::render('devices.create'))
 @section('content')
+    <div class="d-sm-flex mb-4 ml-sm-auto">
+        <a href="{{route('devices.index')}}" class="btn btn-danger btn-icon-split">
+                    <span class="icon text-white-50">
+                      <span class="fas fa-arrow-circle-left"></span>
+                    </span>
+            <span class="text">Torna indietro</span>
+        </a>
+    </div>
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">
@@ -17,7 +25,7 @@
                     @csrf
                     @method('POST')
                     <div class="form-group row">
-                        <label for="inputDeviceId" class="col-sm-4 col-form-label"><i class="fas fa-user"></i> Id dispositivo</label>
+                        <label for="inputDeviceId" class="col-sm-4 col-form-label"><span class="fas fa-microchip"></span> Id dispositivo</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control @error('deviceId') is-invalid @enderror" id="inputDeviceId" placeholder="Id dispositivo" value="" name="deviceid">
                             @error('deviceId')
@@ -28,7 +36,7 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="inputDeviceName" class="col-sm-4 col-form-label"><i class="fas fa-user"></i> Nome dispositivo</label>
+                        <label for="inputDeviceName" class="col-sm-4 col-form-label"><span class="fas fa-tag"></span> Nome dispositivo</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control @error('deviceName') is-invalid @enderror" id="inputDeviceName" placeholder="Nome dispositivo" value="" name="deviceName">
                             @error('deviceName')
@@ -39,7 +47,7 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="inputGatewayName" class="col-sm-4 col-form-label"><i class="fas fa-user"></i> Nome gateway</label>
+                        <label for="inputGatewayName" class="col-sm-4 col-form-label"><span class="fas fa-dungeon"></span> Nome gateway</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control @error('gatewayName') is-invalid @enderror" id="inputGatewayName" placeholder="Nome gateway" value="" name="gatewayName">
                             @error('gatewayName')
@@ -50,24 +58,31 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="inputFrequency" class="col-sm-4 col-form-label"><i class="fas fa-user-tag"></i> Frequenza ricezione dati (richieste / s)</label>
-                        <div class="col-sm-8">
-                            <select class="form-control @error('frequency') is-invalid @enderror" name="frequency" id="inputFrequency">
-                                <option value="0">0.5</option>
-                                <option value="1">1</option>
-                                <option value="2">1.5</option>
-                                <option value="3">2</option>
-                                <option value="4">2.5</option>
-                                <option value="5">3</option>
-                                <option value="6">3.5</option>
-                                <option value="7">4</option>
-                            </select>
-                            @error('frequency')
-                            <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                            @enderror
-                        </div>
+                            <label for="inputFrequency" class="col-sm-4 col-form-label"><span class="fas fa-history"></span> Frequenza ricezione dati</label>
+                            <div class="col-sm-8">
+                                <div class="input-group mb-3">
+                                <select class="form-control @error('frequency') is-invalid @enderror" name="frequency" id="inputFrequency">
+                                    <option >0.5</option>
+                                    <option >1</option>
+                                    <option >1.5</option>
+                                    <option >2</option>
+                                    <option >2.5</option>
+                                    <option >3</option>
+                                    <option >3.5</option>
+                                    <option >4</option>
+                                    <option >4.5</option>
+                                    <option >5</option>
+                                </select>
+                                @error('frequency')
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                                <span class="input-group-append">
+                                    <span class="input-group-text" id="basic-addon2">Secondi</span>
+                                </span>
+                            </div>
+                            </div>
                     </div>
                     <div class="form-group row">
                         <div class="col-sm-4">
