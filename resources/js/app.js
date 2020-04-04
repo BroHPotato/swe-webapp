2d46e406f339c3c1a43581cf1ec758ac2be95b60
Giuseppe Vito Bitetti <giuseppevitobitetti@libero.it>
2020-04-04 15:36:23 +0200

    n'altra prova

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,7 +23,10 @@ window.Vue = require("vue");
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component("chart-management", require("./components/ChartManagement.vue").default);
+Vue.component(
+    "chart-management",
+    require("./components/ChartManagement.vue").default
+);
 
 import VueApexCharts from "vue-apexcharts";
 Vue.use(VueApexCharts);
@@ -36,6 +39,6 @@ Vue.component("apexchart", VueApexCharts);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-let app = new Vue({
+const app = new Vue({
     el: "#app",
 });
