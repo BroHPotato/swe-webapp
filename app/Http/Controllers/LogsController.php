97c6eb5d6c5506cb6f06c94eb622210633bd0025
Giuseppe Vito Bitetti <giuseppevitobitetti@libero.it>
2020-04-08 09:34:57 +0200

    fix

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -6,12 +6,14 @@ use Illuminate\Http\Request;
 
 class LogsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $logs = ["stringa1", "stringa2"];
         view('logs.index', compact('logs'));
     }
 
-    public function show($logId){
+    public function show($logId)
+    {
         $log = "stringa?";
         view('logs.show', compact('log'));
     }
