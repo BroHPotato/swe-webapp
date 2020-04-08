1d6f0b23f88f3d3b3b88c7305d1b7d34a08809ed
Giuseppe Vito Bitetti <giuseppevitobitetti@libero.it>
2020-04-08 09:34:19 +0200

    scaffolding logs

---
+++ app/Http/Controllers/LogsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LogsController extends Controller
+{
+    public function index(){
+        $logs = ["stringa1", "stringa2"];
+        view('logs.index', compact('logs'));
+    }
+
+    public function show($logId){
+        $log = "stringa?";
+        view('logs.show', compact('log'));
+    }
+}
---
+++ app/Providers/LogsServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Gateway;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class LogsServiceProvider extends BasicProvider
+{
+    /**
+     * @var Client
+     */
+    private $request;
+
+    /**
+     * GatewayServiceProvider constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(app());
+        $this->request = new Client([
+            'base_uri' => config('app.api') . '/logs/',
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+    }
+
+
+    /**
+     * @param mixed $identifier
+     * @return Gateway
+     */
+    public function find($identifier)
+    {
+        try {
+            $response = json_decode($this->request->get($identifier, $this->setHeaders())->getBody());
+            return $response;
+        } catch (RequestException $e) {
+            abort($e->getCode(), $e->getResponse()->getReasonPhrase());
+            return null;
+        }
+    }
+
+    /**
+     * @return array|null
+     */
+    public function findAll()
+    {
+        try {
+            $response = json_decode($this->request->get('', $this->setHeaders())->getBody());
+            return $response;
+        } catch (RequestException $e) {
+            abort($e->getCode(), $e->getResponse()->getReasonPhrase());
+            return null;
+        }
+    }
+}
