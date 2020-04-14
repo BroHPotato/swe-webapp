291b318f090221ea84b4971ba1d3f9d8c018284b
Mariano <marianosciacco@gmail.com>
2020-04-14 23:24:03 +0200

    Fix con bitnami laravel

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.16.2-alpine3.11
+FROM bitnami/laravel:6
 COPY . /usr/src/webapp
 WORKDIR /usr/src/webapp
 CMD ["sh", "start.sh"]
--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-apk add composer
-
-composer install
-
-npm install
-
-npm run dev
+echo "Web application starting..."
 
 php artisan serve
 
-echo "Web application started..."
+
